5f872aec740e5819dc0f7c8880b8a53af5105384
AndrewSaunders12 <ninjaraider04@gmail.com>
2024-10-10 20:11:35 -0400

    using operators

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -1,10 +1,37 @@
 ﻿namespace OperatorExercise
+
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int a = 17;
+            int b = 4;
+
+            int sum = a + b;
+
+            int difference = a - b;
+
+            int product = a * b;
+
+            int quotient = a / b;
+
+            int remainder = a % b;
+
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+            Console.WriteLine();
+            Console.WriteLine("What is the radius of a cirle that you would like to use?");
+            double radius = double.Parse(Console.ReadLine());
+            Console.WriteLine(AreaOfCircle(radius));
+
+
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            var area = Math.PI * Math.Pow(radius, 2);
+            return area;
         }
     }
 }
